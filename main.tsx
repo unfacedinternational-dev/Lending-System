1aaa49fd433268bd11a01bae959cfa39ee34d980
unfacedinternational-dev <unfacedinternational@gmail.com>
2026-09-22 15:08:39 +0100

    Add Vite production build configuration

---
+++ main.tsx
@@ -0,0 +1,1 @@
+import React from 'react';import{createRoot}from'react-dom/client';import'./index.css';import App from './finance_hub_of_the_north';createRoot(document.getElementById('root')!).render(<React.StrictMode><App/></React.StrictMode>);
